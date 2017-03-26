fe65588fbfbc4d60a4b016115457f2a01f74dfbd
imse <imse@jacken.se>
2017-03-26 01:11:14 +0100

    Added Wifi login
    You need to add your own file named private.h in the main directory and add the appropriate login info for your own wifi network
    
    Signed-off-by: imse <imse@jacken.se>

--- ESP8266_OLED_Clock.ino
+++ ESP8266_OLED_Clock.ino
@@ -21,6 +21,13 @@ All text above, and the splash screen must be included in any redistribution
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include "./libraries/NTPClient.h"
+
+/*
+Real values included in file private.h
+const char *ssid     = "<SSID>";
+const char *password = "<PASSWORD>"
+*/
+
 #define OLED_RESET 0
 Adafruit_SSD1306 display(OLED_RESET);
 
